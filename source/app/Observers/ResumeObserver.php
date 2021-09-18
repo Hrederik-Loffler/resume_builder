e2b838b8d8f4eeb8daaf87af1b2df27e38703da2
Denis Belik <denis.belik2014@gmail.com>
2021-09-18 14:36:44 +0300

    generate resume with basic data

--- source/app/Observers/ResumeObserver.php
+++ source/app/Observers/ResumeObserver.php
@@ -2,11 +2,22 @@
 
 namespace App\Observers;
 
+use App\Exceptions\Http\InvalidDataException;
+use App\Exceptions\Http\NotFoundException;
 use App\Models\Resume;
+use App\Services\ResumeService;
 use Illuminate\Support\Facades\Storage;
 
 class ResumeObserver
 {
+    /**
+     * @param ResumeService $resumeService
+     */
+    public function __construct(ResumeService $resumeService)
+    {
+        $this->resumeService = $resumeService;
+    }
+
     /**
      * Handle the Resume "deleted" event.
      *
@@ -15,6 +26,23 @@ class ResumeObserver
      */
     public function deleted(Resume $resume)
     {
-        Storage::disk("templates")->delete("$resume->template_path.blade.php");
+        $this->resumeService->deleteTemplate($resume->template_path);
+    }
+
+    /**
+     * Handle the Resume "creating" event. It tests template before inserting it.
+     *
+     * @param Resume $resume
+     */
+    public function creating(Resume $resume)
+    {
+        try {
+            // @NOTE: Validate template with test data before finally saving it.
+            $this->resumeService->generate($resume->template_path, config('constants.resume'));
+        } catch (InvalidDataException | NotFoundException $e) {
+            // @NOTE: Don't forget to delete template in templates folder, because it's invalid.
+            $this->resumeService->deleteTemplate($resume->template_path);
+            throw new InvalidDataException("Your template doesn't follow our rules. Please, refer to documentation.");
+        }
     }
 }
