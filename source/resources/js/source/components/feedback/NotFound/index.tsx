2dbe81ccbb827e1dc6720fcc93016361262d15f1
Denis Belik <denis.belik2014@gmail.com>
2021-10-15 19:15:37 +0300

    display default, white square image for resume preview in browse page

--- source/resources/js/source/components/feedback/NotFound/index.tsx
+++ source/resources/js/source/components/feedback/NotFound/index.tsx
@@ -1,4 +1,7 @@
 // @NOTE: Import from libraries.
+import preview from "@constants/preview";
+
+// @NOTE: Import from own files.
 import { EmptyState } from "@shopify/polaris";
 
 /**
@@ -7,10 +10,5 @@ import { EmptyState } from "@shopify/polaris";
  * @return {JSX.Element}
  */
 export default function NotFound() {
-    return (
-        <EmptyState
-            heading="404 not found"
-            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl9DN9irguvWfsYs5L3jiXISDvycmclpp1HQ&usqp=CAU"
-        />
-    );
+    return <EmptyState heading="404 not found" image={preview.notFound} />;
 }
